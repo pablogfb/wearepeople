2487fed65538765e817424586317745981850d40
pablogfb <pablobuntu@gmail.com>
2016-03-11 19:35:29 +0100

    car class logic implemented

--- cart.rb
+++ cart.rb
@@ -7,6 +7,8 @@ class Cart
   attr_accessor :cart
   def initialize(pricing_rules = nil)
     @cart={}
+    @pricing_rules = pricing_rules
+    @list = {"KR"=> "Kryptonita", "ME" => "Melange", "UN" => "Unobtainium"}
   end
 
   def add(code)
@@ -15,20 +17,11 @@ class Cart
 
   def total
    total = 0
-   if self.cart["KR"] != nil
-     kr = Kryptonita.new self.cart["KR"]
-     total += kr.total
-   end
-   
-   if self.cart["ME"] != nil
-     me = Melange.new self.cart["ME"]
-     total += me.total
-   end
-   
-   if self.cart["UN"] != nil
-     un = Unobtainium.new self.cart["UN"]
-     total += un.total
-   end
+   @cart.each_key {|key| 
+     clazz = Object.const_get(@list[key])
+     x= clazz.new @cart[key], @pricing_rules[key]
+     total += Discounts.calc x
+   }
    total
   end
 end
